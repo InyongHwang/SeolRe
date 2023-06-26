a2f5fd025cd5680d9fe48ff4ba84636b7cd48b2f
InyongHwang <hiy1934@gmail.com>
2023-06-26 22:53:26 +0900

    Initial commit

---
+++ src/main/java/inyonghwang/seolre/SeolReApplication.java
@@ -0,0 +1,13 @@
+package inyonghwang.seolre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeolReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeolReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inyonghwang/seolre/SeolReApplicationTests.java
@@ -0,0 +1,13 @@
+package inyonghwang.seolre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeolReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
